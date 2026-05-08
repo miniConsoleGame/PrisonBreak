c3b9f8785fa5affc476c84e7080e6c744c2ff8a7
신예은 <qkdrn2341@gmail.com>
2026-05-08 17:01:40 +0900

    ConsoleText 추가 , GameManager 삭제, Main 게임로직구현

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,58 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
     boolean isPoliceAlive;
-    GameManager gameManager;
-    Random random;
+    int floor;
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws Exception {
 
         Main main = new Main();
         main.Start();
     }
 
-    private void Start() {
+    private void Start() throws Exception {
         InitGame();
         ConsoleText.Intro();
+
+
         UpdateGame();
-        random = new Random();
-    }
-
-    private void InitGame(){
-        System.out.println("initGame");
-        gameManager = new GameManager(12);
-        gameManager.setFloor();
-        System.out.println(gameManager.getFloor());
-
 
     }
 
-    private void UpdateGame() {
+    private void InitGame() {
+        System.out.println("      ------------InitGame------------");
+        Random random = new Random();
+        floor = random.nextInt(1, 101);
+        System.out.println(floor);
+        isPoliceAlive = true;
+    }
 
-        while (isPoliceAlive)
-        {
+    private void UpdateGame() throws Exception {
+        boolean isFind = false;
+
+        Scanner sc = new Scanner(System.in);
+        int tempFloor;
+
+        while (!isFind) {
+            System.out.print("도둑이 숨은 층을 조사하여 체포 하십시오. ( 1 ~ 100층 )");
+            tempFloor = sc.nextInt();
+            isFind = tempFloor == floor;
+            if (isFind) {
+                ConsoleText.printPoliceWithBaton();
+                ConsoleText.playAnimation();
+                System.out.println("도둑을 발견했다!!\n기절시켜서 감옥에 다시 데려다 놔야 한다 !! 저놈 잡아랏 !! ");
+                break;
+            } else {
+                System.out.println(tempFloor > floor ? "down" : " up");
+            }
+        }
+
+
+        while (isPoliceAlive) {
+
 
         }
     }

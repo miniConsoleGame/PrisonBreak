663f833571856981ef94cf7b3a55ee9485898274
신예은 <qkdrn2341@gmail.com>
2026-05-12 16:35:15 +0900

    Main.java 리팩토링

--- src/game/Main.java
+++ src/game/Main.java
@@ -9,9 +9,9 @@ import java.util.Scanner;
 
 public class Main {
 
-
     int floor;
 
+    private final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
 
@@ -20,67 +20,106 @@ public class Main {
     }
 
     private void Start() throws Exception {
+
         InitGame();
+
         ConsoleText.Intro();
 
-
         UpdateGame();
-
     }
 
     private void InitGame() {
-        System.out.println("      ------------InitGame------------");
-        Random random = new Random();
-        floor = random.nextInt(1, 101);
-        System.out.println(floor);
 
+        System.out.println("      ------------InitGame------------");
+
+        Random random = new Random();
+
+        floor = random.nextInt(1, 101);
+
+        System.out.println(floor);
     }
 
     private void UpdateGame() throws Exception {
-        boolean isFind = false;
 
-        Scanner sc = new Scanner(System.in);
-        int tempFloor;
+        boolean isFind = false;
 
         while (!isFind) {
 
-            System.out.print("도둑이 숨은 층을 조사하여 체포 하십시오. ( 1 ~ 100층 )");
-            tempFloor = sc.nextInt();
-            //1층에서 100층 까지만 
-            if (tempFloor > 100 || tempFloor < 1) {
+            int tempFloor = inputFloor();
+
+            if (!isValidFloor(tempFloor)) {
+
                 System.out.println("1~100 사이의 숫자만 입력해주세요.");
+
                 continue;
             }
+
             isFind = tempFloor == floor;
+
             if (isFind) {
-                ConsoleText.printPoliceWithBaton();
-                ConsoleText.playAnimation();
-                System.out.println("도둑을 발견했다!!\n기절시켜서 감옥에 다시 데려다 놔야 한다 !! 저놈 잡아랏 !! ");
 
-                Police police = new Police();
-                Thief thief = new Thief();
-                BattleManager battleManager = new BattleManager();
-                battleManager.startBattle(police, thief);
+                startBattle();
 
-                break;
             } else {
-                System.out.println(tempFloor > floor ? "down" : " up");
+
+                printHint(tempFloor);
             }
         }
-        EndGame();
 
+        EndGame();
+    }
+
+    private int inputFloor() {
+
+        System.out.print("도둑이 숨은 층을 조사하여 체포 하십시오. ( 1 ~ 100층 )");
+
+        return sc.nextInt();
+    }
+
+    private boolean isValidFloor(int tempFloor) {
+
+        return tempFloor >= 1 && tempFloor <= 100;
+    }
+
+    private void printHint(int tempFloor) {
+
+        System.out.println(tempFloor > floor ? "down" : "up");
+    }
+
+    private void startBattle() throws Exception {
+
+        ConsoleText.printPoliceWithBaton();
+
+        ConsoleText.playAnimation();
+
+        System.out.println("""
+                도둑을 발견했다!!
+                기절시켜서 감옥에 다시 데려다 놔야 한다 !!
+                저놈 잡아랏 !!
+                """);
+
+        Police police = new Police();
+
+        Thief thief = new Thief();
+
+        BattleManager battleManager = new BattleManager();
+
+        battleManager.startBattle(police, thief);
     }
 
     private void EndGame() {
 
         if (BattleManager.IsPoliceAlive()) {
+
             ConsoleText.printPoliceWithBaton();
+
             System.out.println("잡았다 이놈!");
+
         } else {
+
             ConsoleText.printThiefRunaway();
-            System.out.println("아이쿠 ! 놓쳐버렸네 ! ");
+
+            System.out.println("아이쿠 ! 놓쳐버렸네 !");
         }
     }
-
-
 }
